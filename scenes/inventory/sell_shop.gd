6091e5d735e3cb8c9be286e19be731318bf355de
rkpham <ryankpham@gmail.com>
2024-12-19 14:47:58 -0600

    Fix farm encounter

--- scenes/inventory/sell_shop.gd
+++ scenes/inventory/sell_shop.gd
@@ -10,6 +10,8 @@ func _on_sell_button_pressed() -> void:
 		if item_slot.item_button.item.can_be_sold:
 			item_slot.item_button.queue_free()
 			GameState.gold += item_slot.item_button.item.price
+			if text_timeout:
+				text_timeout.disconnect("timeout", _on_sell_info_timeout)
 			sell_info.text = "Sold for %d gold." % item_slot.item_button.item.price
 			text_timeout = get_tree().create_timer(4)
 			text_timeout.timeout.connect(func(): sell_info.text = "")

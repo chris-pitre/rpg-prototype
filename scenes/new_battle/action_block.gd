efcae874703a487e0d72f291a91e1214fb16ac6b
rkpham <ryankpham@gmail.com>
2024-12-26 10:40:33 -0600

    windup/active/recovery visual and data

--- scenes/new_battle/action_block.gd
+++ scenes/new_battle/action_block.gd
@@ -3,7 +3,8 @@ extends Control
 
 signal delete_pressed(this: ActionBlock)
 
-@export var action: BattleAction
+@export var action: BattleAction:
+	set = _set_action
 
 func _ready() -> void:
 	size_flags_stretch_ratio = action.length
@@ -15,3 +16,10 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 	set_drag_preview(preview)
 	
 	return ActionDragData.new(self, action, preview)
+
+func _set_action(_action: BattleAction) -> void:
+	action = _action
+	$NameLabel.text = _action.name
+	$HBoxContainer/Windup.size_flags_stretch_ratio = _action.windup
+	$HBoxContainer/Active.size_flags_stretch_ratio = _action.active
+	$HBoxContainer/Recovery.size_flags_stretch_ratio = _action.recovery
